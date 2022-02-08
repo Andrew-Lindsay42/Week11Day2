0c2ce7c98fe60820bfd873133cb457704c91a255
Andrew Lindsay <andrew_e_lindsay@hotmail.com>
2022-02-08 16:51:53 +0000

    Added tests for borrow and return methods

--- src/test/java/BorrowerTest.java
+++ src/test/java/BorrowerTest.java
@@ -4,10 +4,17 @@ import static org.junit.Assert.assertEquals;
 
 public class BorrowerTest {
     Borrower borrower;
+    Library library;
+    Book hobbit;
+    Book dune;
 
     @Before
     public void before(){
         borrower = new Borrower("Andrew");
+        hobbit = new Book("The Hobbit", "J.R.R. Tolkien", "Fantasy");
+        dune = new Book("Dune", "Frank Herbert", "Sci-Fi");
+        library = new Library("Library of Alexandria", 3);
+        library.addBook(hobbit);
     }
 
     @Test
@@ -18,5 +25,38 @@ public class BorrowerTest {
     @Test
     public void cardStartsAtZero(){
         assertEquals(0, borrower.checkCard());
+    }
+
+    @Test
+    public void canBorrowBook(){
+        borrower.borrowBook(hobbit, library);
+        assertEquals(1, borrower.checkCard());
+        assertEquals(0, library.checkStock());
+    }
+
+    @Test
+    public void cannotBorrowBookIfNotInLibrary(){
+        borrower.borrowBook(dune, library);
+        assertEquals(0, borrower.checkCard());
+        assertEquals(1, library.checkStock());
+    }
+
+    @Test
+    public void canReturnBook(){
+        library.addBook(dune);
+        borrower.borrowBook(hobbit, library);
+        borrower.borrowBook(dune, library);
+        assertEquals(2, borrower.checkCard());
+        assertEquals(0, library.checkStock());
+        borrower.returnBook(hobbit, library);
+        assertEquals(1, borrower.checkCard());
+        assertEquals(1, library.checkStock());
+    }
+
+    @Test
+    public void cannotReturnBookIfNotOnCard(){
+        borrower.returnBook(dune, library);
+        assertEquals(0, borrower.checkCard());
+        assertEquals(1, library.checkStock());
     }
 }
