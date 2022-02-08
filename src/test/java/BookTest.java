0ac500ede23e4a8e64fd6d2d5a2e58237eb2c660
Andrew Lindsay <andrew_e_lindsay@hotmail.com>
2022-02-08 15:47:15 +0000

    Added tests for Book Class

--- src/test/java/BookTest.java
+++ src/test/java/BookTest.java
@@ -1,2 +1,27 @@
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
 public class BookTest {
+    Book book;
+
+    @Before
+    public void before(){
+        book = new Book("The Hobbit", "J.R.R. Tolkien", "Fantasy");
+    }
+
+    @Test
+    public void hasTitle(){
+        assertEquals("The Hobbit", book.getTitle());
+    }
+
+    @Test
+    public void hasAuthor(){
+        assertEquals("J.R.R. Tolkien", book.getAuthor());
+    }
+
+    @Test
+    public void hasGenre(){
+        assertEquals("Fantasy", book.getGenre());
+    }
 }
